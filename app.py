fa3faf881f4f6085ece475c2db71dd5ebf24b320
Nalin Rajendran <nalinrajendran1@gmail.com>
2023-10-09 22:55:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import toml
 import streamlit as st
 from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
 import os
-import PyPDF2
+# import PyPDF2
 import pypdf
 
 os.environ["OPENAI_API_KEY"] = st.secrets["secrets"]["OPENAI_API_KEY"]
