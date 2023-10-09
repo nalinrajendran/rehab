b3a4e76a85c053ed614ff3c4778c9299b510e98f
Nalin Rajendran <nalinrajendran1@gmail.com>
2023-10-09 23:29:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,35 @@ from pypdf import PdfReader
 
 os.environ["OPENAI_API_KEY"] = st.secrets["secrets"]["OPENAI_API_KEY"]
 
+# @st.cache_resource(experimental_allow_widgets=True)
+# def load_data():
+#     documents = SimpleDirectoryReader('data').load_data()
+#     return GPTVectorStoreIndex.from_documents(documents)
+
+# index = load_data()
+
+# st.title("Rehab Document Query Engine")
+# user_input = st.text_input("Ask a question about the AKPMR document:")
+
+# if user_input:
+#     result = index.query(user_input)
+#     st.write(result)
+
+
 @st.cache_resource(experimental_allow_widgets=True)
 def load_data():
     documents = SimpleDirectoryReader('data').load_data()
-    return GPTVectorStoreIndex.from_documents(documents)
+    return VectorStoreIndex.from_documents(documents)
 
 index = load_data()
+query_engine = index.as_query_engine()  # Create a query engine from the index
 
 st.title("Rehab Document Query Engine")
 user_input = st.text_input("Ask a question about the AKPMR document:")
 
 if user_input:
-    result = index.query(user_input)
+    result = query_engine.query(user_input)  # Use the query method on the query engine object
     st.write(result)
-
 
 
 
